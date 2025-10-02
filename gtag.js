2be5f08315acffcbea7c5e868acf7afdc3308a89
TabBastard <tabzillionaire@gmail.com>
2025-10-02 12:43:45 +0800

    Add files via upload

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EHS6J41QP1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EHS6J41QP1');
+</script>
